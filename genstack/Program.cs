ee7a0e38087698a4d40e26bdc4e0d5755ffbf6a9
Zelenková Markéta <zelenkova.ma.2019@skola.ssps.cz>
2021-09-20 12:58:49 +0200

    Přidejte soubory projektu.

---
+++ genstack/Program.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace genstack
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+    class Stack<T>
+    {
+        T[] elements;
+        int index;
+        public Stack(int maxElementCount = 100)
+        {
+            elements = new T[maxElementCount];
+            index = -1;
+        }
+        public void Push(T t)
+        {
+            elements[++index] = t;
+        }
+        public T Pop()
+        {
+            if (index>=0)
+            {
+                T t = elements[index];
+                index--;
+                return t;
+            }
+            else throw new IndexOutOfRangeException();
+        }
+        public T Peek()
+        {
+            T t = elements.Last();
+            return t;
+        }
+        public bool IsEmpty()
+        {
+            bool b;
+            if (elements.Count() == 0) b = true;
+            else b = false;
+            return b;
+        }
+        public string PrintStack()
+        {
+            string s = "";
+            foreach (T item in elements)
+            {
+                s += $"{item} ";
+            }
+            return s;
+        }
+    }
+}
